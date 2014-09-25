c9f5f73df4eb548aa799fb2e1412330518f9ba1c
eshyong <eric.shyong@gmail.com>
2014-09-25 12:04:52 -0700

    Added emit_member_table

--- common-func.php
+++ common-func.php
@@ -9,4 +9,30 @@ function rank_str($rank) {
 		return floatval($kyu) . 'k';
 	}
 }
+
+function emit_member_table($num_rows, $num_cols, $name, $display, $query) {
+    echo "<table id=\"$name\" style=\"display:$display\"><tr>";
+	for ($col = 0; $col < $num_cols; $col++) {
+	echo "<td valign=top>\n"
+	   . "<table border=1>"
+	   . "<tr><td>Name (Rating)</td>"
+	   . "<td width=30 align=center>I Am Here</td></tr>";
+
+	for ($row = 0; $row < $num_rows; $row++) {
+		// Emit tables for the all member table
+		$result = mysql_fetch_array($query);
+		if ($result) {
+			echo "<tr><td>"
+			   . $result['name']
+			   . "(" . rank_str($result['rank']) . ")"
+			   . "<td align=center><input type=checkbox" . " onclick=\"reg_member(this, " . $result['id'] . ")\"></td>"
+			   . "</tr>\n";
+		}
+	}
+	echo "</table>\n" 
+	   . "</td>\n";
+}
+echo "</tr></table>\n";
+
+}
 ?>
