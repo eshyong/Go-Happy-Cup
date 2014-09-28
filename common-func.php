7656de5b36ea7472cf1a3ff0f27845150dcb9f88
eshyong <eric.shyong@gmail.com>
2014-09-28 14:11:14 -0700

    Changed expanded whitespace tabs to regular tabs

--- common-func.php
+++ common-func.php
@@ -11,31 +11,31 @@ function rank_str($rank) {
 }
 
 function emit_member_table($total_num, $num_cols, $name, $display, $query) {
-    // Calculate number of rows to emit.
-    $num_rows = $total_num / $num_cols;
+	// Calculate number of rows to emit.
+	$num_rows = $total_num / $num_cols;
 
-    // Emit the outer table.
-    echo "<table id=\"$name\" style=\"display:$display\"><tr>";
+	// Emit the outer table.
+	echo "<table id=\"$name\" style=\"display:$display\"><tr>";
 	for ($col = 0; $col < $num_cols; $col++) {
-        echo "<td valign=top>\n"
-           . "<table border=1>"
-           . "<tr><td>Name (Rating)</td>"
-           . "<td width=30 align=center>I Am Here</td></tr>";
+		echo "<td valign=top>\n"
+		   . "<table border=1>"
+		   . "<tr><td>Name (Rating)</td>"
+		   . "<td width=30 align=center>I Am Here</td></tr>";
 
-        for ($row = 0; $row < $num_rows; $row++) {
-            // Emit tables for the all member table
-            $result = mysql_fetch_array($query);
-            if ($result) {
-                echo "<tr><td>"
-                   . $result['name']
-                   . "(" . rank_str($result['rank']) . ")"
-                   . "<td align=center><input type=checkbox" . " onclick=\"reg_member(this, " . $result['id'] . ")\"></td>"
-                   . "</tr>\n";
-            }
-        }
-        echo "</table>\n" 
-           . "</td>\n";
-    }
-    echo "</tr></table>\n";
+		for ($row = 0; $row < $num_rows; $row++) {
+			// Emit tables for the all member table
+			$result = mysql_fetch_array($query);
+			if ($result) {
+				echo "<tr><td>"
+				   . $result['name']
+				   . "(" . rank_str($result['rank']) . ")"
+				   . "<td align=center><input type=checkbox" . " onclick=\"reg_member(this, " . $result['id'] . ")\"></td>"
+				   . "</tr>\n";
+			}
+		}
+		echo "</table>\n" 
+		   . "</td>\n";
+	}
+	echo "</tr></table>\n";
 }
 ?>
