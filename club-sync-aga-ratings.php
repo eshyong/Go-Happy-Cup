1be237d2b21f5dfb24c4851156fea9f3788f45f0
eshyong <eric.shyong@gmail.com>
2014-09-28 12:21:59 -0700

    Sync club members' ranks if their AGA ratings are updated. This resolves part of eshyong/go-happy-cup#1.

--- club-sync-aga-ratings.php
+++ club-sync-aga-ratings.php
@@ -1,41 +1,76 @@
 <?php
-require('config.php');
-private function download_file($path, $url) {
-	$newfname = $path;
-	$file = fopen ($url, "rb");
-	if ($file) {
-		$newf = fopen ($newfname, "wb");
+require_once('config.php');
+// TODO: Find out how to download files using HTTP for better error handling.
+function download_file($path, $url) {
+    // Ask the website for a file.
+	$download = fopen ($url, "rb");
+	if (!$download) {
+        // HTTP request failed somehow.
+        echo "Request failed";
+        return false;
+    }
 
-		if ($newf)
-			while(!feof($file)) {
-				fwrite($newf, fread($file, 1024 * 8 ), 1024 * 8 );
-			}
-	}
+    $localfile = fopen ($path, "wb");
+    if (!$localfile) {
+        // Some system error.
+        echo "Error creating file";
+        return false;
+    }
+    while(!feof($download)) {
+        // Read in chunks.
+        fwrite($localfile, fread($download, 1024 * 8 ), 1024 * 8 );
+    }
 
-	if ($file) {
-		fclose($file);
-	}
-
-	if ($newf) {
-		fclose($newf);
-	}
+    // Close our file descriptors.
+    fclose($download);
+    fclose($localfile);
+    return true;
 }
 
-function download_aga_file($aga_ratings_txt)
-{
-	// download_file($path, "https://usgo.org/mm/tdlista.txt");
-	$newf = fopen ($newfname, "wb");
-	if ($newf) {
-		fwrite($newf, AGA_RATING_NAMES);
-		fclose($newf);
-	}
+function update_aga_ranks($path) {
+    // Get today's date as a timestamp.
+    $today = strtotime('today');
+
+    // DB configurations.
+    $conn = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PW);
+    mysql_select_db(MYSQL_DB, $conn);
+    $update_query = 'UPDATE members SET rank=$rating WHERE aga_id=$id';
+
+    $contents = file_get_contents($path);
+    if ($contents) {
+        // Split contents by line, and leave off last empty item.
+        $lines = explode("\n", $contents);
+        $lines = array_slice($lines, 0, count($lines) - 1);
+
+        // Get each line as an $entry array.
+        foreach($lines as $line) {
+            // Each CSV line is delimited by tabs, so we use preg_split to turn it into an array.
+            $entry = preg_split("/\t/", $line);
+            $name = $entry[0];
+            $id = $entry[1];
+            $rating = $entry[3];
+            $expiration = $entry[4];
+
+            // Validate $id and $rating as numbers, and update player rank if aga_id matches.
+            if (is_numeric($id) && is_numeric($rating) && strtotime($expiration) > $today) {
+                $query = str_replace(['$rating', '$id'], [$rating, $id], $update_query);
+                $result = mysql_query($query, $conn);
+                echo "name: $name; id: $id; rating: $rating; expiration date: $expiration\n";
+            }
+        }
+    }
 }
 
 function main()
 {
-	$aga_ratings_txt = "/tmp/gohappycup_tdlista.txt";
-	download_aga_file($aga_ratings_txt);
-	import_aga_ratings($aga_ratings_txt);
+    date_default_timezone_set('America/Los_Angeles');
+    $url = 'https://usgo.org/mm/tdlista.txt';
+    $path = 'tmp_ratings.txt';
+    if (!download_file($path, $url)) {
+        echo "Unable to download file.";
+        return;
+    }
+    update_aga_ranks($path);
 }
 
 main();
